3e41181525e4014be0e8b5ceccbf6d896113da6a
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-06-18 23:49:08 +0300

    Create yandex.ts

---
+++ src/lib/yandex.ts
@@ -0,0 +1,8 @@
+export const YANDEX_API_URL =
+  "https://llm.api.cloud.yandex.net/foundationModels/v1/completion";
+
+export const YANDEX_API_KEY =
+  process.env.YANDEX_API_KEY!;
+
+export const YANDEX_FOLDER_ID =
+  process.env.YANDEX_FOLDER_ID!;
