3589164bb5d15c4f21a4dcce64dbc449af43e60a
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-06-21 22:51:48 +0300

    Create soul.ts

---
+++ src/types/soul.ts
@@ -0,0 +1,17 @@
+export interface SoulScanResult {
+  archetype: string;
+  emotion: string;
+  insight: string;
+}
+
+export interface DreamResult {
+  summary: string;
+  emotion: string;
+  interpretation: string;
+}
+
+export interface TarotResult {
+  card: string;
+  meaning: string;
+  guidance: string;
+}
