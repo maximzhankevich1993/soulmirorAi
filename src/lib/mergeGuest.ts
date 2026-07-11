1d6fefa4ca086ec549534d197716a20658575f67
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-07-11 09:26:50 +0300

    Create mergeGuest.ts

---
+++ src/lib/mergeGuest.ts
@@ -0,0 +1,12 @@
+import { prisma } from "@/lib/prisma";
+
+export async function mergeGuestToUser(
+  guestId: string,
+  userId: string
+) {
+  // сюда позже перенесем:
+  // Soul Scan
+  // Dream
+  // Tarot
+  // Usage
+}
