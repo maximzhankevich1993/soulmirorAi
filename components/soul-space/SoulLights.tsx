7471e40d250b2f63dd1eff893a5df41d018d8872
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-07-30 12:20:25 +0300

    Create SoulLights.tsx

---
+++ components/soul-space/SoulLights.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { PointLight } from "@react-three/drei";
+
+
+interface SoulLightsProps {
+
+  color: string;
+
+}
+
+
+
+export function SoulLights({
+  color,
+}: SoulLightsProps) {
+
+
+  return (
+
+    <>
+
+      {/* Main golden cinematic light */}
+
+      <pointLight
+
+        position={[
+          3,
+          3,
+          3,
+        ]}
+
+        intensity={4.5}
+
+        color={color}
+
+        distance={8}
+
+      />
+
+
+
+      {/* Purple secondary atmosphere */}
+
+      <pointLight
+
+        position={[
+          -3,
+          -2,
+          2,
+        ]}
+
+        intensity={3}
+
+        color="#8B5CF6"
+
+        distance={7}
+
+      />
+
+
+
+      {/* Soft front illumination */}
+
+      <pointLight
+
+        position={[
+          0,
+          1,
+          4,
+        ]}
+
+        intensity={1.5}
+
+        color="#F4F1EA"
+
+        distance={6}
+
+      />
+
+
+
+      {/* Ambient cinematic fill */}
+
+      <ambientLight
+
+        intensity={0.8}
+
+      />
+
+    </>
+
+  );
+
+}
