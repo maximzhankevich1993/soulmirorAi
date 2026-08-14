8273812554e2743430c9fc0510708408ff2882ae
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-08-14 14:06:30 +0300

    Update SoulAwakening.tsx

--- src/components/soul-space/SoulAwakening.tsx
+++ src/components/soul-space/SoulAwakening.tsx
@@ -28,22 +28,15 @@ export function SoulAwakening({
 
   return (
     <>
-      <AnimatePresence mode="wait">
+      <AnimatePresence>
         {loading && (
           <motion.div
             key="soulmirror-loader"
             className="fixed inset-0 z-[9999]"
-            initial={{
-              opacity: 1,
-            }}
-            animate={{
-              opacity: 1,
-            }}
-            exit={{
-              opacity: 0,
-            }}
+            initial={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
             transition={{
-              duration: 1.2,
+              duration: 1.4,
               ease: [0.22, 1, 0.36, 1],
             }}
           >
@@ -53,14 +46,12 @@ export function SoulAwakening({
       </AnimatePresence>
 
       <motion.div
-        initial={{
-          opacity: 0,
-        }}
+        initial={{ opacity: 0 }}
         animate={{
           opacity: loading ? 0 : 1,
         }}
         transition={{
-          duration: 1.1,
+          duration: 1.4,
           ease: [0.22, 1, 0.36, 1],
         }}
       >
