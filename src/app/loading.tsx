4a51ca5e8a5321c0d2ad4fedaf29d3a532376e54
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-08-14 14:13:30 +0300

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import { CinematicLoader } from "../../components/loading/CinematicLoader";
 
+const MIN_LOADING_TIME = 4000;
+
 export default function Loading() {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    const timer = window.setTimeout(() => {
+      setReady(true);
+    }, MIN_LOADING_TIME);
+
+    return () => {
+      window.clearTimeout(timer);
+    };
+  }, []);
+
+  if (!ready) {
+    return <CinematicLoader />;
+  }
+
   return <CinematicLoader />;
 }
