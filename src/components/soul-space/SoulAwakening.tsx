5ffa7bb3160dd0c7e39c93cc4b577ea8b8bbd88d
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-08-14 14:15:40 +0300

    Update SoulAwakening.tsx

--- src/components/soul-space/SoulAwakening.tsx
+++ src/components/soul-space/SoulAwakening.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { motion } from "framer-motion";
-
 interface SoulAwakeningProps {
   children: React.ReactNode;
 }
@@ -9,20 +7,5 @@ interface SoulAwakeningProps {
 export function SoulAwakening({
   children,
 }: SoulAwakeningProps) {
-  return (
-    <motion.div
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-      }}
-      transition={{
-        duration: 1.2,
-        ease: [0.22, 1, 0.36, 1],
-      }}
-    >
-      {children}
-    </motion.div>
-  );
+  return <>{children}</>;
 }
