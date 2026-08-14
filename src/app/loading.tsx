a4c099f93099281ee1ef9d7841020156b093805d
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-08-14 14:11:15 +0300

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,4 @@
-import { CinematicLoader } from "../../../components/loading/CinematicLoader";
+import { CinematicLoader } from "../../components/loading/CinematicLoader";
 
 export default function Loading() {
   return <CinematicLoader />;
